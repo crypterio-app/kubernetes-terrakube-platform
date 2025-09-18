7e5cd2cca5c12835f8715cd45ce7c619c84865a1
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-09-19 00:39:27 +0300

    update

--- modules/terrakube/main.tf
+++ modules/terrakube/main.tf
@@ -4,7 +4,11 @@ resource "helm_release" "terrakube" {
   namespace  = "terrakube"
   repository = "https://terrakube-io.github.io/terrakube-helm-chart"  # official Helm repo
   chart      = "terrakube"
-  #version    = "3.27.6"  # replace with latest stable version
+  version    = "3.27.6"  # replace with latest stable version
+
+  atomic          = false   # prevents automatic rollback on timeout
+  cleanup_on_fail  = true
+  timeout         = 900     # 15 min, Airflow may take long to deploy
 
   create_namespace = true
   values = [
